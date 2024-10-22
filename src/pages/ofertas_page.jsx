0e806c3d60db44762f1a5ed98cd8c328f9b9bcdf
valnhe <vpazcastle@gmail.com>
2024-10-22 20:08:59 -0300

    add new pages

---
+++ src/pages/ofertas_page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const OfertasPage = () => {
+  return (
+    <>
+      <p>ofertasss</p>
+    </>
+  )
+}
+
+export default OfertasPage
---
+++ src/pages/postulaciones_page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const PostulacionesPage = () => {
+  return (
+    <>
+      <p>Postulacioness</p>
+    </>
+  )
+}
+
+export default PostulacionesPage
