95a90d7e83834b2278b7fbd5aea9bc91612a9fed
NicolasArmijoC <nicolas.armijoc@sansano.usm.cl>
2024-10-26 20:48:22 -0300

    update ofertas_page

--- src/pages/ofertas_page.jsx
+++ src/pages/ofertas_page.jsx
@@ -3,24 +3,34 @@ import Ofertas from "../mockup.json";
 import Card from "../components/card-ofertas";
 
 export const OfertasPage = () => {
+
+  // Temas de responsive design
+
+  const [ofertasPorPagina, setofertasPorPagina] = useState(4);
+
+  const calculateItemsPerPage = () => {
+    const screenWidth = window.innerWidth;
+
+    if (screenWidth >= 1300) {
+      setofertasPorPagina(12); 
+    } else if (screenWidth >= 900) {
+      setofertasPorPagina(6); 
+    } else if (screenWidth >= 600) {
+      ssetofertasPorPagina(6); 
+    } else {
+      setofertasPorPagina(4);
+    }
+  };
+
+  useEffect(() => {
+    calculateItemsPerPage();
+    window.addEventListener('resize', calculateItemsPerPage);
+    return () => window.removeEventListener('resize', calculateItemsPerPage);
+  }, []);
+
+  // Paginación de Ofertas
+
   const [currentPage, setCurrentPage] = useState(1);
-  const [searchTerm, setSearchTerm] = useState("");
-  const ofertasPorPagina = 12;
-
-  const filteredOfertas = searchTerm ? Ofertas.filter((oferta) => 
-    (oferta.puesto && oferta.puesto.toLowerCase().includes(searchTerm.trim().toLowerCase())) ||
-    (oferta.empresa && oferta.empresa.toLowerCase().includes(searchTerm.trim().toLowerCase())) ||
-    (oferta.ubicacion && oferta.ubicacion.toLowerCase().includes(searchTerm.trim().toLowerCase())) ||
-    (oferta.modalidad && oferta.modalidad.toLowerCase().includes(searchTerm.trim().toLowerCase()))
-  ) 
-  
-  : Ofertas;
-
-  const indexOfLastOferta = currentPage * ofertasPorPagina;
-  const indexOfFirstOferta = indexOfLastOferta - ofertasPorPagina;
-  const currentOfertas = filteredOfertas.slice(indexOfFirstOferta, indexOfLastOferta);
-
-  const totalPages = Math.ceil(filteredOfertas.length / ofertasPorPagina);
 
   const handleNextPage = () => {
     if (currentPage < totalPages) {
@@ -40,23 +50,98 @@ export const OfertasPage = () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   }, [currentPage]);
 
+  const [searchTerm, setSearchTerm] = useState("");
+  const [tipo, setTipo] = useState([]);
+  const [modalidad, setModalidad] = useState([]);
+
+  useEffect(() => {
+    setCurrentPage(1);
+  }, [modalidad, tipo, searchTerm]);
+
+  // Filtrado de Ofertas
+
+  const handleModalidad = (mod) => {
+    if (!modalidad.includes(mod)) {
+      setModalidad([...modalidad, mod]);
+    } else {
+      setModalidad(modalidad.filter((m) => m !== mod));
+    }
+  };
+
+  const handleTipo = (tip) => {
+    if (!tipo.includes(tip)) {
+      setTipo([...tipo, tip]);
+    } else {
+      setTipo(tipo.filter((t) => t !== tip));
+    }
+  };
+
+  const filteredOfertas = Ofertas.filter((oferta) => {
+    const matchesSearchTerm = searchTerm
+        ? (oferta.puesto && oferta.puesto.toLowerCase().includes(searchTerm.trim().toLowerCase())) ||
+          (oferta.empresa && oferta.empresa.toLowerCase().includes(searchTerm.trim().toLowerCase())) ||
+          (oferta.ubicacion && oferta.ubicacion.toLowerCase().includes(searchTerm.trim().toLowerCase())) ||
+          (oferta.modalidad && oferta.modalidad.toLowerCase().includes(searchTerm.trim().toLowerCase()))
+        : true;
+
+    const tip = oferta.tipo;
+    const matchesModalidad = modalidad.length ? modalidad.includes(oferta.modalidad) : true;
+    const matchesTipo = tipo.length ? (tipo.includes(tip[0]) || tipo.includes(tip[1])): true;
+
+    return matchesSearchTerm && matchesModalidad && matchesTipo;
+  });
+
+  const indexOfLastOferta = currentPage * ofertasPorPagina;
+  const indexOfFirstOferta = indexOfLastOferta - ofertasPorPagina;
+  const currentOfertas = filteredOfertas.slice(indexOfFirstOferta, indexOfLastOferta);
+
+  const totalPages = Math.ceil(filteredOfertas.length / ofertasPorPagina);
+
   return (
     <main className='ofertas-page'>
       <h1>Ofertas de Prácticas</h1>
       <p>En esta página se mostrarán las ofertas de prácticas disponibles para postular.</p>
 
       <input
-        type="text"
-        placeholder="Buscar ofertas..."
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-        className="ofertas-page__search" 
+          type="text"
+          placeholder="Buscar ofertas..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className="ofertas-page__search" 
       />
+      
+      <section className='ofertas-page__filters'>
+        <aside>
+          <h2>Filtros</h2>
+            <div>
+              <h3>Tipo de práctica</h3>
+              <label>
+                <input type="checkbox" id="cbox1" value="first_checkbox" onChange={() => handleTipo('Industrial')}/> Industrial
+              </label>
+              <label>
+                <input type="checkbox" id="cbox2" value="first_checkbox" onChange={() => handleTipo('Profesional')}/> Profesional
+              </label>
+            </div>
 
-      <section className='ofertas-page__section'>
-        {currentOfertas.map((oferta) => (
-          <Card key={oferta.id} Oferta={oferta} />
-        ))}
+          <div>
+            <h3>Modalidad</h3>
+            <label>
+              <input type="checkbox" id="cbox3" value="first_checkbox" onChange={() => handleModalidad('Presencial')} /> Presencial
+            </label>
+            <label>
+              <input type="checkbox" id="cbox4" value="first_checkbox" onChange={() => handleModalidad('Híbrido')} /> Híbrido
+            </label>
+            <label>
+              <input type="checkbox" id="cbox5" value="first_checkbox" onChange={() => handleModalidad('Remoto')} /> Remoto
+            </label>
+          </div>
+        </aside>
+
+        <section className='ofertas-page__section'>
+          {currentOfertas.map((oferta) => (
+            <Card key={oferta.id} Oferta={oferta} />
+          ))}
+        </section>
       </section>
 
       <div className="ofertas-page__pagination">
