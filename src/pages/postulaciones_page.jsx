5e744483f7910bb9755a4d0ede2be35a6d210811
GasparNavarro <gaspar.navarro@usm.cl>
2024-10-28 07:20:16 -0300

    postulaciones_page

--- src/pages/postulaciones_page.jsx
+++ src/pages/postulaciones_page.jsx
@@ -2,9 +2,41 @@ import React from 'react'
 
 export const PostulacionesPage = () => {
   return (
-    <>
-      <p>Postulacioness</p>
-    </>
+    <main className='postulaciones-page'>
+      <h1>Mis Postulaciones</h1>
+      <p>Aquí encontrarás información sobre tus postulaciones.</p>
+
+      <div>
+        <section className='postulaciones-page__grid'>
+          <h2>Empresa</h2>
+          <h2>Fecha de Postulación</h2>
+          <h2>Estado</h2>
+
+          <p>Tech Innovators</p>
+          <p>22/12/2022</p>
+          <p className='postulaciones-page--rev'>En revisión</p>
+
+          <p>BioTech Solutions</p>
+          <p>13/11/2022</p>
+          <p className='postulaciones-page--aceptada'>Aceptada</p>
+
+          <p>CyberSecurity Ltd.</p>
+          <p>05/10/2022</p>
+          <p className='postulaciones-page--rechazada'>Rechazada</p>
+
+          <p>IoT Solutions</p>
+          <p>30/09/2022</p>
+          <p className='postulaciones-page--rechazada'>Rechazada</p>
+
+
+          <p>Gaming Innovators</p>
+          <p>15/09/2022</p>
+          <p className='postulaciones-page--rechazada'>Rechazada</p>
+        </section>
+      </div>
+
+
+    </main>
   )
 }
 
