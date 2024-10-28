08391167359e0650f615c7a8a9fdd5fa18890947
valnhe <vpazcastle@gmail.com>
2024-10-28 19:31:59 -0300

    add new state

--- src/pages/postulaciones_page.jsx
+++ src/pages/postulaciones_page.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export const PostulacionesPage = () => {
+  const [postulacion, setPostulacion] = useState(true)
+
   return (
     <main className='postulaciones-page'>
       <h1>Mis Postulaciones</h1>
@@ -11,27 +13,36 @@ export const PostulacionesPage = () => {
           <h2>Empresa</h2>
           <h2>Fecha de Postulación</h2>
           <h2>Estado</h2>
+          <h2></h2>
 
           <p>Tech Innovators</p>
           <p>22/12/2022</p>
-          <p className='postulaciones-page--rev'>En revisión</p>
+          {postulacion && <p>Postulación enviada</p>}
+          {!postulacion && <p>Cancelada</p>}
+
+          {postulacion && <p className='postulaciones-page--solicitud' onClick={() => setPostulacion(false)}>Cancelar postulación</p>}
+          {!postulacion && <p></p>}
 
           <p>BioTech Solutions</p>
           <p>13/11/2022</p>
           <p className='postulaciones-page--aceptada'>Aceptada</p>
+          <p></p>
 
           <p>CyberSecurity Ltd.</p>
           <p>05/10/2022</p>
-          <p className='postulaciones-page--rechazada'>Rechazada</p>
+          <p className='postulaciones-page--rev'>Rechazada</p>
+          <p></p>
 
           <p>IoT Solutions</p>
           <p>30/09/2022</p>
           <p className='postulaciones-page--rechazada'>Rechazada</p>
+          <p></p>
 
 
           <p>Gaming Innovators</p>
           <p>15/09/2022</p>
           <p className='postulaciones-page--rechazada'>Rechazada</p>
+          <p></p>
         </section>
       </div>
 
